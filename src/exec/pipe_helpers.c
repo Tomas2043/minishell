e06b85c2872d5c9ae1e464e85de21165421b7b2c
Tomás Andrade <tomas.andrade2043@gmail.com>
2026-04-16 14:37:31 +0100

    Fixed norminette issues

---
+++ src/exec/pipe_helpers.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe_helpers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: toandrad <toandrad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/13 14:52:59 by toandrad          #+#    #+#             */
+/*   Updated: 2026/04/16 10:55:54 by toandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/minishell.h"
+
+int	count_cmds(t_cmd *cmd)
+{
+	int	count;
+
+	count = 0;
+	while (cmd)
+	{
+		count++;
+		cmd = cmd->next;
+	}
+	return (count);
+}
+
+void	free_pipes(int **pipes, int count)
+{
+	int	i;
+
+	i = 0;
+	while (i < count)
+	{
+		free(pipes[i]);
+		i++;
+	}
+	free(pipes);
+}
